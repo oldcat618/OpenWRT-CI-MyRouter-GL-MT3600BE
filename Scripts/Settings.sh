1be32ad9c2690b0ea536f8e654436a68a954effc
yjy116 <yjy116@gmail.com>
2026-03-26 11:04:10 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Default LAN IP
-sed -i 's/192\.168\.1\.1/192.168.8.1/g' package/base-files/files/bin/config_generate || true
-
-# Default hostname
-sed -i "s/hostname='.*'/hostname='MT3600BE'/g" package/base-files/files/bin/config_generate || true
-
-# Default theme
-find package feeds -type f -path "*/collections/luci/Makefile" -exec sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' {} + || true
+# Basic customization only; avoid touching theme defaults that depend on external feeds.
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate || true
+sed -i "s/hostname='OpenWrt'/hostname='MT3600BE'/g" package/base-files/files/bin/config_generate || true
