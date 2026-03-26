67e9fdc82fd921fad39b1b92232476a8554e62ba
yjy116 <yjy116@gmail.com>
2026-03-26 09:07:26 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-# 追加常用第三方软件源
+# 仅保留当前项目真正会用到的额外主题源，减少 feeds 风险。
 cat >> feeds.conf.default <<'FEEDS'
 src-git argon https://github.com/jerrykuku/luci-theme-argon.git
-src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git
 FEEDS
