b31ed2be10e15d16c4f41ed4b7c3ccf1c9a312e8
oldcat <wwwcat@126.com>
2026-04-01 13:58:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,4 +6,4 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 2. 【核心变更】确保 HomeProxy 使用 ImmortalWrt 原生推荐版本
 # 这样可以避免编译时出现 sing-box 依赖版本不匹配的报错
 rm -rf package/feeds/luci/luci-app-homeproxy
-git clone https://github.com/immortalwrt/homeproxy package/homeproxy
+#git clone https://github.com/immortalwrt/homeproxy package/homeproxy
